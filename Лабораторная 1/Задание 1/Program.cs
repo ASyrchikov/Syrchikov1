773af62f8d786c0de3b44979b775fb947aeed942
ASyrchikov <atalanta009@gmail.com>
2017-10-17 20:32:26 +0600

    Лабораторная 1. Решил задания 1 2 3 4 5

---
+++ Лабораторная 1/Задание 1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("+---+"); 
+             Console.WriteLine("|   |"); 
+             Console.WriteLine("|   |"); 
+             Console.WriteLine("+---+");
+             Console.WriteLine("#");
+             Console.WriteLine("##");
+             Console.WriteLine("###");
+             Console.WriteLine("####");
+             Console.WriteLine("/\\");
+             Console.WriteLine("//\\\\");
+             Console.WriteLine("//\\\\");
+             Console.WriteLine("][");
+             Console.WriteLine("{ \"x\":15, \"y\": 28.7 }");
+             Console.WriteLine("Три девицы под окном");
+             Console.WriteLine("Пряли поздно вечерком.");
+             Console.WriteLine("\"Кабы я была царица");
+             Console.WriteLine("Говорит одна девица, —");
+             Console.WriteLine("То на весь крещеный мир");
+             Console.WriteLine("Приготовила б я пир\".");
+             Console.WriteLine("\"Кабы я была царица, —");
+             Console.WriteLine("Говорит ее сестрица, —");
+            Console.WriteLine("То на весь бы мир одна");
+            Console.WriteLine("Наткала я полотна\".");
+            
+
+        }
+    }
+}
---
+++ Лабораторная 1/Задание 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String a;
+                a = Console.ReadLine();
+            Console.Write("Мы стремимся к ");
+            Console.Write(a);
+
+
+        }
+    }
+}
