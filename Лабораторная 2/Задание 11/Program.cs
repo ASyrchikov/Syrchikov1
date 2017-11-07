fce2affdc2b957c21e49f520744a8356e163930d
Anton <atalanta009@gmail.com>
2017-11-07 20:29:13 +0600

    Лабораторная 2 Задания 11-17 Без 16го

---
+++ Лабораторная 2/Задание 11/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            a = Console.ReadLine();
+            double x;
+            x = Double.Parse(a);
+           
+
+            if (x < 0)
+            {
+                Console.WriteLine("Значение должно быть неотрицательным");
+                return;
+            }
+           
+            double result;
+            result = Math.Sqrt(x);
+            Console.WriteLine("{0:F4}", result);
+        }
+    }
+}
+         
+            
+            
+     
---
+++ Лабораторная 2/Задание 15/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            a = Console.ReadLine();
+            double x;
+            x = Double.Parse(a);
+            if (x < -273.15)
+                {
+                Console.WriteLine("Температура должна быть выше абсолютного нуля");
+                return;
+            }
+            double y;
+            y = x * 1.8;
+            double result;
+            result = y + 32;
+            Console.WriteLine("{0:F4}", result);
+
+        }
+    }
+}
